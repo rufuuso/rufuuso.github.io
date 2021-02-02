7a85b92757fc92dc378f949f6c67535658942852
rufuuso <63652572+rufuuso@users.noreply.github.com>
2021-02-02 18:29:27 -0300

    Add files via upload
    new

--- js/main.js
+++ js/main.js
@@ -27,3 +27,23 @@ function toggleMenu(){
     }
 
 }
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
